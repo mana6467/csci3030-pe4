9705ef36e50bee66c2b2d42a603ae21527c1b213
mana6467@colorado.edu <mana6467@colorado.edu>
2021-02-17 05:42:03 +0000

    Added GetPlayerChoice function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,6 +15,17 @@ void DisplayBoard(std::string Board[3][3]){
     std::cout << Board[2][0] << " | " << Board[2][1] << " | " << Board[2][2] << " | " << std::endl;
 }
 
+int GetPlayerChoice(){
+    int choice;
+    std::cout << "Pick a square to mark. (Numbered 1-9 s.t. row 1 = sqaures [1][2][3] etc.)" << std::endl;
+    std::cin >> choice;
+    while (choice < 1 || choice > 9){
+        std::cout << "Invalid choice, pick an unpicked square (numbered 1-9 respectively)" << std::endl;
+        std::cin >> choice;
+    }
+    return choice;
+}
+
 int main(){
     std::string tttBoard[3][3];
     CreateBoard(tttBoard);
