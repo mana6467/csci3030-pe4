8c144b922f4a611d27a2368b294a598799b1c321
mana6467@colorado.edu <mana6467@colorado.edu>
2021-02-17 05:56:07 +0000

    PlaceMarker function added

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,6 +15,38 @@ void DisplayBoard(std::string Board[3][3]){
     std::cout << Board[2][0] << "  " << Board[2][1] << "  " << Board[2][2] << "  " << std::endl;
 }
 
+void PlaceMarker(int square, std::string marker, std::string Board[3][3]){
+    switch (square) {
+        case 1:
+            Board[0][0] = marker;
+            break;
+        case 2:
+            Board[0][1] = marker;
+            break;
+        case 3:
+            Board[0][2] = marker;
+            break;
+        case 4:
+            Board[1][0] = marker;
+            break;
+        case 5:
+            Board[1][1] = marker;
+            break;
+        case 6:
+            Board[1][2] = marker;
+            break;
+        case 7:
+            Board[2][0] = marker;
+            break;
+        case 8:
+            Board[2][1] = marker;
+            break;
+        case 9:
+            Board[1][2] = marker;
+            break; 
+    }
+}
+
 int main(){
     std::string tttBoard[3][3];
     CreateBoard(tttBoard);
